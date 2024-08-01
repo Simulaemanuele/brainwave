3a60cf873657fac584a8af43cf3d39c749501285
Emanuele Simula <simulaemanuele@gmail.com>
2024-08-01 16:40:32 +0200

    feat: created design folder in components created Benefits.jsx file with GradientLights custom component

---
+++ src/components/design/Benefits.jsx
@@ -0,0 +1,5 @@
+export const GrandientLights = () => {
+  return (
+    <div className="absolute top-0 left-1/4 w-full aspect-square bg-radial-gradient from-[#28206C] to-[#28206C]/0 to-60% pointer-events-none" />
+  );
+};
