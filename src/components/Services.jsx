4f874ce9bd154458f5476c372ea7e07000816575
Emanuele Simula <simulaemanuele@gmail.com>
2024-08-05 17:07:14 +0200

    feat: finished the Services.jsx custom component section develop.

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import Section from "./Section";
 import Heading from "./Heading";
 import { service1, service2, service3, check } from "../assets";
-import { brainwaveServices } from "../constants";
+import { brainwaveServices, brainwaveServicesIcons } from "../constants";
 import Generating from "./Generating";
+import {
+  PhotoChatMessage,
+  Gradient,
+  VideoBar,
+  VideoChatMessage,
+} from "./design/Services";
 
 const Services = () => {
   return (
@@ -45,7 +51,7 @@ const Services = () => {
             <Generating className="absolute left-4 right-4 bottom-4 border border-n-1/10 lg:left-1/2 lg-right-auto lg:bottom-8 lg:-translate-x-1/2" />
           </div>
 
-          <div className="relative z-1 grid lg:grid-cols-2 ">
+          <div className="relative z-1 grid gap-5 lg:grid-cols-2 ">
             <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden">
               <div className="absolute inset-0">
                 <img
@@ -63,8 +69,53 @@ const Services = () => {
                   photo editing feature. Try it now!
                 </p>
               </div>
+              <PhotoChatMessage />
+            </div>
+            <div className="p-4 bg-n-7 rounded-3xl overflow-hidden lg:min-h-[46rem]">
+              <div className="py-12 px-4 xl:px-8">
+                <h4 className="h4 mb-4">Video generation</h4>
+                <p className="body-2 mb-[2rem] text-n-3">
+                  The world&apos;s most powerful AI photo and video art
+                  generation engine. What will you create?
+                </p>
+                <ul className="flex items-center justify-between">
+                  {brainwaveServicesIcons?.map((icon, idx) => (
+                    <li
+                      key={idx}
+                      className={`rounded-2xl flex items-center justify-center ${
+                        idx === 2
+                          ? "w-[3rem] h-[3rem] p-0.25 bg-conic-gradient md:w-[4.5rem] md:h-[4.5rem]"
+                          : "flex w-10 h-10 bg-n-6 md:w-15 md:h-15"
+                      }`}
+                    >
+                      <div
+                        className={`${
+                          idx === 2
+                            ? "flex items-center justify-center w-full h-full bg-n-7 rounded-[1rem]"
+                            : ""
+                        }`}
+                      >
+                        <img src={icon} width={24} height={24} alt={icon} />
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              <div className="relative h-[20rem] bg-n-8 rounded-xl overflow-hidden md:h-[25rem]">
+                <img
+                  src={service3}
+                  className="w-full h-full object-cover"
+                  width={520}
+                  height={400}
+                  alt="Scary robot"
+                />
+                <VideoChatMessage />
+                <VideoBar />
+              </div>
             </div>
           </div>
+          <Gradient />
         </div>
       </div>
     </Section>
