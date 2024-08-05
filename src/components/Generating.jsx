e87f893e1e30067fb6b8e18933607f173a1d1b80
Emanuele Simula <simulaemanuele@gmail.com>
2024-08-05 16:17:15 +0200

    feat: implemented first image of the bento design and the second too with text in overlay and responsive according to the device. TODO: add the last service section and implement graphics details

--- src/components/Generating.jsx
+++ src/components/Generating.jsx
@@ -9,7 +9,7 @@ const Generating = ({ className }) => {
       } text-base`}
     >
       <img className="w-5 h-5 mr-4" src={loading} alt="Loading" />
-      AI is generating
+      AI is generating|
     </div>
   );
 };
