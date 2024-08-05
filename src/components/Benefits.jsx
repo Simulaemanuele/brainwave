8a7a4b8a4d9ba65265d67c01ba8c177267c0aa83
Emanuele Simula <simulaemanuele@gmail.com>
2024-08-05 12:09:04 +0200

    feat: finished developing the Benefits.jsx section custom components

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -3,6 +3,8 @@ import Section from "./Section";
 import Heading from "./Heading";
 import { benefits } from "../constants";
 import Arrow from "../assets/svg/Arrow";
+import { GradientLights } from "../components/design/Benefits";
+import ClipPath from "../assets/svg/ClipPath";
 
 const Benefits = () => {
   return (
@@ -31,6 +33,24 @@ const Benefits = () => {
                   <Arrow />
                 </div>
               </div>
+              {item?.light && <GradientLights />}
+              <div
+                className="absolute inset-0.5 bg-n-8"
+                style={{ clipPath: "url(#benefits)" }}
+              >
+                <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
+                  {item?.imageUrl && (
+                    <img
+                      src={item?.imageUrl}
+                      width={380}
+                      height={362}
+                      alt={item?.title}
+                      className="w-full h-full object-cover"
+                    />
+                  )}
+                </div>
+              </div>
+              <ClipPath />
             </div>
           ))}
         </div>
--- src/components/design/Benefits.jsx
+++ src/components/design/Benefits.jsx
@@ -1,4 +1,4 @@
-export const GrandientLights = () => {
+export const GradientLights = () => {
   return (
     <div className="absolute top-0 left-1/4 w-full aspect-square bg-radial-gradient from-[#28206C] to-[#28206C]/0 to-60% pointer-events-none" />
   );
